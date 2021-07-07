cc4cc7ca1207e5617e7d27bcd9aa85a388f45925
Sachman19 <sachindmishra@knights.ucf.edu>
2021-07-07 04:18:29 -0400

    start

--- src/test/java/ucf/assignments/AppTest.java
+++ src/test/java/ucf/assignments/AppTest.java
@@ -4,6 +4,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     public void testing(){
-
+        /*
+        A user shall be able to add a new todo list
+            Test size of arrayList. if arraylist length is +1 from start, then success
+        A user shall be able to remove an existing todo list
+            Test size of arrayList. if arraylist length is -1 from start, then success
+        A user shall be able to edit the title of an existing todo list
+            write a test to compare string of todoList title.
+        A user shall be able to add a new item to an existing todo list
+            Test size of arrayList. if arraylist length is +1 from start, then success
+        A user shall be able to remove an item from an existing todo list
+            Test size of arrayList. if arraylist length is -1 from start, then success
+        A user shall be able to edit the description of an item within an existing todo list
+            Compare string of description
+        A user shall be able to edit the due date of an item within an existing todo list
+            Compare string of due date
+        A user shall be able to mark an item in a todo list as complete
+            Compare completion integer values
+        A user shall be able to display all of the existing items in a todo list
+        A user shall be able to display only the incompleted items in a todo list
+        A user shall be able to display only the completed items in a todo list
+        A user shall be able to save all of the items in a single todo list to external storage
+            Check for file creation.
+        A user shall be able to save all of the items across all of the todo lists to external storage
+            Check for file creation, +length?
+        A user shall be able to load a single todo list that was previously saved to external storage
+            Check arraylist after loading file
+        A user shall be able to load multiple todo lists that were previous saved to external storage
+            Check arraylist after loading file
+         */
     }
 }
